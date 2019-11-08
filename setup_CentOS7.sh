2b4c5331d353d1867f91fabce76686296aabc266
Steven Green <sg1sg2sg3@hotmail.co.uk>
2019-11-08 15:25:44 +0000

    Modifications to ensure model works on CentOS7.

---
+++ setup_CentOS7.sh
@@ -0,0 +1,8 @@
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+
+setup boost v1_56_0 -q e6:prof
+setup clhep v2_3_4_6 -q e17:prof
+setup cmake v3_14_3 -f Linux64bit+3.10-2.17
+setup gcc v7_3_0 -f Linux64bit+3.10-2.17
+setup geant4 v4_10_3_p03c -f Linux64bit+3.10-2.17 -q e17:prof
+setup root v6_16_00 -f Linux64bit+3.10-2.17 -q e17:prof
